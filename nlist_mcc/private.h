29b01546058fa6641d867f3738b863a57de8f5ec
Thore Böckelmann <tboeckel@gmx.de>
2010-05-07 09:14:54 +0000

    * nlist_mcc/NList_mcc4.c: simplified the image spec handling. Specifying   additional dimensions was discouraged anyway and the old scanning  algorithm   completely broke the color handling. Effectively this fixes bug #1272693.

--- nlist_mcc/private.h
+++ nlist_mcc/private.h
@@ -623,4 +623,11 @@ ULONG xget(Object *obj, const IPTR attr);
 #endif
 ///
 
+#ifndef MIN
+  #define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+#ifndef MAX
+  #define MAX(a,b) (((a) > (b)) ? (a) : (b))
+#endif
+
 #endif /* MUI_NList_priv_MCC_H */
