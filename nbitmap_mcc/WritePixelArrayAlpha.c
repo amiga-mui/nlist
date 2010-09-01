3d90ce83ccb2b80f50d912e5d67fbe4b9280d1a7
Thore Böckelmann <tboeckel@gmx.de>
2010-09-01 08:53:32 +0000

    * nbitmap_mcc/WritePixelArrayAlpha.c: fixed the final image calculation by   combining the correct RGB values. This makes it possible to correctly   display alpha blended images even with V42 CGX/OS3 systems.

--- nbitmap_mcc/WritePixelArrayAlpha.c
+++ nbitmap_mcc/WritePixelArrayAlpha.c
@@ -54,7 +54,7 @@ void _WPAA(APTR src, UWORD srcx, UWORD srcy, UWORD srcmod, struct RastPort *rp, 
         spix = (ULONG *)((ULONG)src + (srcy + y) * srcmod + srcx * sizeof(ULONG));
         dpix = buf;
 
-        for (x = 0; x < width; x++)
+        for(x = 0; x < width; x++)
         {
           ULONG srcpix, dstpix, a, r, g, b;
 
@@ -68,7 +68,7 @@ void _WPAA(APTR src, UWORD srcx, UWORD srcy, UWORD srcmod, struct RastPort *rp, 
 
           a = a - globalalpha;
 
-          if (a > 0)
+          if(a > 0)
           {
             ULONG dest_r, dest_g, dest_b;
 
@@ -80,7 +80,7 @@ void _WPAA(APTR src, UWORD srcx, UWORD srcy, UWORD srcmod, struct RastPort *rp, 
             dest_g += do_alpha(a, g - dest_g);
             dest_b += do_alpha(a, b - dest_b);
 
-            dstpix = 0xff000000 | r << 16 | g << 8 | b;
+            dstpix = 0xff000000 | dest_r << 16 | dest_g << 8 | dest_b;
           }
 
           *dpix++ = dstpix;
