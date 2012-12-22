589b5b66871ad43b29bcd9fd12c28b55de2da80a
Jens Maus <mail@jens-maus.de>
2012-12-22 20:35:13 +0000

    * misc: final v0.115 release preparations

--- nfloattext_mcc/version.h
+++ nfloattext_mcc/version.h
@@ -31,8 +31,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    19
-#define LIB_REVISION   69
-#define LIB_DATE       "20.12.2012"
+#define LIB_REVISION   70
+#define LIB_DATE       "22.12.2012"
 #define LIB_COPYRIGHT  "Copyright (C) 2007-2012 NList Open Source Team"
 
 // set the LIB_REV_STRING
--- nlist_mcc/version.h
+++ nlist_mcc/version.h
@@ -32,8 +32,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    20
-#define LIB_REVISION   133
-#define LIB_DATE       "20.12.2012"
+#define LIB_REVISION   134
+#define LIB_DATE       "22.12.2012"
 #define LIB_COPYRIGHT  "Copyright (C) 2001-2012 NList Open Source Team"
 
 // set the LIB_REV_STRING
