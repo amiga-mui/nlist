ad9ec23f8c6fa568a09ae4b1f62bed12afdc8d66
Jens Maus <mail@jens-maus.de>
2015-03-21 22:32:23 +0000

    * misc: final v0.120 release preparations

--- nlisttree_mcp/version.h
+++ nlisttree_mcp/version.h
@@ -29,8 +29,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    18
-#define LIB_REVISION   33
-#define LIB_DATE       "05.04.2014"
+#define LIB_REVISION   34
+#define LIB_DATE       "21.03.2015"
 #define LIB_COPYRIGHT  "Copyright (C) 2001-2015 NList Open Source Team"
 
 // set the LIB_REV_STRING
