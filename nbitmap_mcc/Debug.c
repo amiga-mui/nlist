b2c54e15cc510e2f77e83c2cc36bc539874a4d5f
Thore Böckelmann <tboeckel@gmx.de>
2016-08-15 11:40:49 +0200

    * nbitmap_mcc/Debug.c: removed duplicate static qualifier.

--- nbitmap_mcc/Debug.c
+++ nbitmap_mcc/Debug.c
@@ -213,7 +213,7 @@ void CleanupDebug(void)
 
 /****************************************************************************/
 
-STATIC static INLINE void _INDENT(void)
+STATIC INLINE void _INDENT(void)
 {
   int i;
 
